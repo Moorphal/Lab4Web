3ac82a078f89b9106897bac7b93544c3c84a31ae
Moorphal <90373463+Moorphal@users.noreply.github.com>
2022-04-13 17:22:54 +0700

    summary

---
+++ webLab2.1/webLab2.1/Controllers/ControlsServiceController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using webLab2._1.Models;
+
+namespace webLab2._1.Controllers
+{
+    public class ControlsServiceController : Controller
+    {
+        private readonly Random _random = new Random();                
+
+        private readonly ILogger<ControlsServiceController> _logger;
+
+        public ControlsServiceController(ILogger<ControlsServiceController> logger)
+        {
+            _logger = logger;                    
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult TextBox(string State)
+        {
+
+
+            if (Request.Method == "POST")
+            {
+                ViewBag.Type = "Text Box";
+                ViewBag.Name = "Text";
+                ViewBag.Value = State;
+                return View("Result");
+            }
+            else
+                return View();
+        }
+
+        public IActionResult TextArea(string State)
+        {
+
+
+            if (Request.Method == "POST")
+            {
+                ViewBag.Type = "Text Area";
+                ViewBag.Name = "Text";
+                ViewBag.Value = State;
+                return View("Result");
+            }
+            else
+                return View();
+        }
+
+        public IActionResult CheckBox(string State)
+        {
+
+
+            if (Request.Method == "POST")
+            {
+                ViewBag.Type = "Check Box";
+                ViewBag.Name = "IsSelected";
+                ViewBag.Value = State;
+                return View("Result");
+            }
+            else
+                return View();
+        }
+        public IActionResult Radio(string State)
+        {
+
+
+            if (Request.Method == "POST")
+            {
+                ViewBag.Type = "Radio";
+                ViewBag.Name = "Month";
+                ViewBag.Value = State;
+                return View("Result");
+            }
+            else
+                return View();
+        }
+
+        public IActionResult DropDownList(string State)
+        {
+
+
+            if (Request.Method == "POST")
+            {
+                ViewBag.Type = "Drop-Down List";
+                ViewBag.Name = "Month";
+                ViewBag.Value = State;
+                return View("Result");
+            }
+            else
+                return View();
+        }
+
+        public IActionResult ListBox(string State)
+        {
+
+
+            if (Request.Method == "POST")
+            {
+                ViewBag.Type = "List Box";
+                ViewBag.Name = "Month";
+                ViewBag.Value = State;
+                return View("Result");
+            }
+            else
+                return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            QuizModel qModel = QuizModel.Instance;
+            qModel.Reset();
+            qModel.Start();
+
+
+            return View(qModel);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(QuizModel qModel, string action)
+        {
+            qModel = QuizModel.Instance;
+
+            if (Request.Form["Answer"] != "") qModel.UserAnswer = Int32.Parse(Request.Form["Answer"]);
+            else qModel.UserAnswer = 0;
+            qModel.Questions();
+
+
+
+            if (action == "Next")
+            {
+                QuizModel quModel = QuizModel.Instance;
+                quModel.Start();
+
+                return View(quModel);
+            }
+            return RedirectToAction("QuizResult");            
+
+        }
+        public IActionResult QuizResult()
+        {
+            QuizModel qModel = QuizModel.Instance;
+            ViewBag.Result = qModel.AllAnswers;
+            ViewData["Всего"] = "" + qModel.Count;
+            ViewData["Правильно"] = "" + qModel.CountOfRightAnswers;
+            return View();
+        }                
+    }
+}
---
+++ webLab2.1/webLab2.1/Controllers/MockupsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Diagnostics;
+using webLab2._1.Models;
+
+namespace webLab2._1.Controllers
+{
+    public class MockupsController : Controller
+    {
+        private readonly ILogger<MockupsController> _logger;
+
+        public MockupsController(ILogger<MockupsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult PassReset()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PassReset(Data User, string action)
+        {
+            if (ModelState["Email"].ValidationState == ModelValidationState.Valid)
+            {
+                if (action == "Send me a code")
+                {
+                    ViewBag.Code = "Your code: " + User.Code;
+                    return View(User);
+                }
+                return Redirect("Confirm");
+            }
+            return View();
+        }
+
+        public IActionResult Confirm(string Value)
+        {
+            Data User = new Data();
+            if (Request.Method == "POST")
+            {
+                if ((User.Code).ToString() == Value)
+                    ViewBag.Check = "You can Reset your Password";
+                else
+                    ViewBag.Check = "Wrong code";
+                return View();
+            }
+            else
+                return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(Data inf)
+        {
+            if (ModelState["FirstName"].ValidationState == ModelValidationState.Valid &
+                ModelState["LastName"].ValidationState == ModelValidationState.Valid &
+                ModelState["Gender"].ValidationState == ModelValidationState.Valid)
+                return RedirectToAction("SignUpCont", inf);
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SignUpCont()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUpCont(Data inf)
+        {
+
+            if (ModelState["Email"].ValidationState == ModelValidationState.Valid &
+                ModelState["Password"].ValidationState == ModelValidationState.Valid &
+                ModelState["ComparePassword"].ValidationState == ModelValidationState.Valid)
+                return View("Result", inf);
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
